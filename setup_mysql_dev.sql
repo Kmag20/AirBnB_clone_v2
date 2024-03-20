a9b15cc3cf12e323644fb7cfde6dc890a4edf016
Kmag20 <owenmawira@gmail.com>
2024-03-20 10:57:39 +0300

    MYSQL Setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create a user hbnb_dev in localhost
-CREATE USER IF NOT EXISTS 'hbnb'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges on the database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
