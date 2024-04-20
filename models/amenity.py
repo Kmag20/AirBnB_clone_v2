8b7da7336e38f77b7d310a3ff2f48f0cee29dd68
Kmag20 <owenmawira@gmail.com>
2024-04-20 13:26:31 +0300

    Amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table
+from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
+    """ Defines the Amenity class"""
+    from models import storage_t
+    if storage_t == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    # name = ""
--- models/place.py
+++ models/place.py
@@ -3,8 +3,14 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, Integer, ForeignKey, Float
+from sqlalchemy import Table, Column, String, Integer, ForeignKey, Float
 from sqlalchemy.orm import relationship
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+
 
 
 class Place(BaseModel, Base):
@@ -23,6 +29,7 @@ class Place(BaseModel, Base):
         latitude = Column(Float)
         longitude = Column(Float)
         reviews = relationship("Review", backref="place", cascade="all, delete, delete-orphan")
+        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
 
     else:
         city_id = ""
@@ -51,3 +58,15 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
+        
+        @property
+        def amenities(self):
+            """getter attribute returns the list of Amenity instances"""
+            from models.amenity import Amenity
+            from models.__init__ import storage
+            amenity_list = []
+            all_amenities = storage.all(Amenity)
+            for amenity in all_amenities.values():
+                if amenity.place_id == self.id:
+                    amenity_list.append(amenity)
+            return amenity_list
