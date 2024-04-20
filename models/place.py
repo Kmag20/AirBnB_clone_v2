b5fad4af3872059efdb8619936676517dfee2449
Kmag20 <owenmawira@gmail.com>
2024-04-20 12:43:07 +0300

    Review

--- models/place.py
+++ models/place.py
@@ -22,6 +22,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float)
         longitude = Column(Float)
+        reviews = relationship("Review", backref="place", cascade="all, delete, delete-orphan")
+
     else:
         city_id = ""
         user_id = ""
@@ -36,3 +38,16 @@ class Place(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    if storage_t != "db":
+        @property
+        def reviews(self):
+            ''' Getter attrib returning the lost of review instances for file storage'''
+            from models.review import Review
+            from models.__init__ import storage
+            review_list = []
+            all_reviews = storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
--- models/review.py
+++ models/review.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
     """ Review classto store review information """
+    from models import storage_t
+    if storage_t == "db":
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    # place_id = ""
-    # user_id = ""
-    # text = ""
