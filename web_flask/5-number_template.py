465da43cf26a14be35e38099ab1b1e6232e31725
Kmag20 <owenmawira@gmail.com>
2024-04-20 16:03:50 +0300

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,10 +40,10 @@ def number(n):
     return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=None):
     ''' Display a HTML page if n is an int '''
-    return render_template('number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
