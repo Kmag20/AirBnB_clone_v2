2af9af7c5c745c3442ddec930a606066b4c28c93
Kmag20 <owenmawira@gmail.com>
2024-04-20 14:01:23 +0300

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,4 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_bnb():
-    return "Hello HBNB"
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', strict_slashes=False)
