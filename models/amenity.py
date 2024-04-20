102d2d578abac0802b14adcf82cb1551e81ec27c
Kmag20 <owenmawira@gmail.com>
2024-04-20 13:27:48 +0300

    Amenities

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Table
 from sqlalchemy.orm import relationship
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
-
 
 class Amenity(BaseModel, Base):
     """ Defines the Amenity class"""
