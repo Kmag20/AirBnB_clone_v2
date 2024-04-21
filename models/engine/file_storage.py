d2b0a15779f57a1adaab72bcba4b1499f2a4dafb
Kmag20 <owenmawira@gmail.com>
2024-04-21 10:19:49 +0300

    List on states

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,8 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        FileStorage.__objects.update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        FileStorage.__objects.update({obj.to_dict()['__class__']
+                                      + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -66,6 +67,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        FileStorage.__objects[key] = classes[val['__class__']](**val)
+                    FileStorage.__objects[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
