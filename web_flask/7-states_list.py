e015db43e011d6844dca32d1a149618ac0fdbc74
Kmag20 <owenmawira@gmail.com>
2024-04-21 10:37:03 +0300

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,12 +4,14 @@
 from flask import Flask, render_template
 from models.__init__ import storage
 
+
 app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     ''' displays some HTML text '''
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown(exception):
