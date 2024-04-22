7029ff19870314252320a7882e7b50fa3c76a602
Kmag20 <owenmawira@gmail.com>
2024-04-22 18:10:11 +0300

    Changes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,9 +3,10 @@
 from flask import Flask, render_template
 import sys
 import os
-PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(PARENT_DIR)
 from models import storage
+pathA = os.path.join(os.path.dirname(__file__), os.pardir)
+PARENT_DIR = os.path.abspath(pathA)
+sys.path.append(PARENT_DIR)
 
 app = Flask(__name__)
 
